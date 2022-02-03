889acbe2649cb9f0608a241a85dced8c73977071
Oh_My_Goat <daftouney@gmail.com>
2022-02-03 11:44:29 +0100

    feat(Home): Fin config première route + ajout composant Home

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
+// Imports de React et React Native :
 import React from 'react';
 import {View, Text} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
 
-export default function App() {
+// Import des dépendences React Navigation :
+// Doc : https://reactnavigation.org/docs/getting-started
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+// Création des stacks
+const Stack = createNativeStackNavigator();
+
+// Component imports :
+import Home from './src/Components/Pages/Home/Home';
+
+export const App = () => {
   return (
+    // Balise englobante pour les stacks
     <NavigationContainer>
-      <View>
-        <Text>Coucou</Text>
-      </View>
+      {/* Déclaration de la navigation */}
+      <Stack.Navigator>
+        {/*
+         Routes : 
+          - name: Nom de la page (affiché)
+          - component: composant cible
+         */}
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
+
+export default App;
---
+++ src/Components/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export const Home = () => {
+  return (
+    <View>
+      <Text>Je suis la Homepage</Text>
+    </View>
+  );
+};
+
+export default Home;
