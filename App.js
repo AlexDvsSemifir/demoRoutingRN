67d35fe0eb41313689c7621184843e3ed972ab89
Oh_My_Goat <daftouney@gmail.com>
2022-02-03 11:23:53 +0100

    :sparkle: initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>Coucou</Text>
+      </View>
+    </NavigationContainer>
+  );
+}
